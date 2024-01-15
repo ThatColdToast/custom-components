80747c8a377797f785d5a8e8fad1b2402fe446ac
Toast <thatcoldtoast@gmail.com>
2024-01-15 06:10:30 -0600

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{collections::HashSet, borrow::Borrow};
+
 fn main() {
-    println!("Hello, world!");
+    let paths = std::fs::read_dir("./pages/").unwrap();
+
+    let mut in_flight: HashSet<&str> = HashSet::new();
+
+    for path in paths {
+        println!("Page: {}", path.as_ref().unwrap().path().display());
+        if in_flight.contains(path.as_ref().unwrap().path().to_str().unwrap()) {
+            return;
+        }
+        
+        in_flight.insert(path.as_ref().unwrap().path().to_str().unwrap());
+        let file = std::fs::read_to_string(path.unwrap().path()).unwrap();
+        
+    }
 }
