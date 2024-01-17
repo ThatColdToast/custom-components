d8bdf0171a7031ca9f841f35cf6a66013267931d
Toast <thatcoldtoast@gmail.com>
2024-01-16 18:05:01 -0600

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use std::{collections::HashSet, borrow::Borrow};
+use std::{collections::HashSet, path::PathBuf};
 
 fn main() {
     let paths = std::fs::read_dir("./pages/").unwrap();
 
-    let mut in_flight: HashSet<&str> = HashSet::new();
-
     for path in paths {
-        println!("Page: {}", path.as_ref().unwrap().path().display());
-        if in_flight.contains(path.as_ref().unwrap().path().to_str().unwrap()) {
-            return;
-        }
-        
-        in_flight.insert(path.as_ref().unwrap().path().to_str().unwrap());
-        let file = std::fs::read_to_string(path.unwrap().path()).unwrap();
-        
+        let mut in_flight: HashSet<String> = HashSet::new();
+        let filepath = path.unwrap().path();
+
+        println!("Page: {}", &filepath.display());
+
+        resolve(&mut in_flight, &filepath.to_str().unwrap().to_string());
+
+        //if in_flight.contains(&filepath.to_str().unwrap()) {
+        //    return;
+        //}
+
+        //in_flight.insert(&filepath.to_str().unwrap());
+        //let filedata = std::fs::read_to_string(&filepath.to_str().unwrap()).unwrap();
+        //println!("{}", filedata);
     }
+}
+
+fn resolve(in_flight: &mut HashSet<String>, filepath: &String) -> String {
+    if in_flight.contains(filepath) {
+        panic!("custom-components does not support recursive types");
+    }
+
+    in_flight.insert(filepath.to_string());
+    let filedata = std::fs::read_to_string(&filepath).unwrap();
+//    println!("{}", &filedata);  
+//    filedata.find("use");
+    String::from(&filedata)
 }
